b70d1e225fd0f575c1088f82a9ce5a8c54e50023
saara <saara>
2018-05-16 18:31:35 +0300

    add test dispatch

--- src/cljs/my_re_frame/events.cljs
+++ src/cljs/my_re_frame/events.cljs
@@ -3,7 +3,14 @@
             [my-re-frame.db :as db]
             ))
 
+
 (re-frame/reg-event-db
  ::initialize-db
  (fn [_ _]
    db/default-db))
+
+
+(re-frame/reg-event-db
+  ::name-change
+  (fn [db [_ new-name-value]]
+    (assoc db :name new-name-value)))   ;; compute and return the new application state
